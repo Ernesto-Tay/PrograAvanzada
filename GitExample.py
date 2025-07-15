8d624d5b1121776f1425965c32b553fc2d3cb9a1
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-15 09:36:45 -0600

    aañadido ciclos IF

--- GitExample.py
+++ GitExample.py
@@ -1,4 +1,9 @@
 import re
 elotes=input("Elotes a 2x1\n ¿Quiere uno o no hay webos?: ").lower()
-if elotes:
+if elotes=="si":
     print("Elotes :D")
+if elotes=="no":
+    print("Triste u-u")
+else:
+    print("No entendí")
+
