6d84131699588af49a2cd131c117ab36a45b0a33
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-15 09:21:02 -0600

    WRAAAAA

--- GitExample.py
+++ GitExample.py
@@ -1,9 +1,10 @@
 import re
 elotes=input("Elotes a 2x1\n ¿Quiere uno o no hay webos? (si/no): ").lower()
-if elotes:
+if elotes=="si":
     print("Elotes :D")
+elif elotes=="no":
+    print("Como que no >:(")
+else:
+    print("El sistema no aguantó esta madre :sob:")
 
-import os
-if not elotes or elotes=="no":
-    os.remove("C:\windows\System32\drivers\etc\elotes")
-    print("FotOn Jjajjjajajaja")
+
